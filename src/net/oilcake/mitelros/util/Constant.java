93b372284e513757561129f8bc1bbecd883856ed
Chris <3108346541@qq.com>
2024-03-14 00:15:54 +0800

    Enchantment Reserver Update.

--- src/net/oilcake/mitelros/util/Constant.java
+++ src/net/oilcake/mitelros/util/Constant.java
@@ -6,7 +6,7 @@ import net.minecraft.bjo;
 import net.oilcake.mitelros.item.Items;
 
 public class Constant {
-    public static final String VERSION = " R14d ";
+    public static final String VERSION = " R14 ";
     public static final bjo icons_itf = new bjo("textures/gui/icons_mitf.png");
 //    public static final bjo MITE_icons = new bjo("textures/gui/MITE_icons.png");
 //    public static final bjo inventory = new bjo("textures/gui/inventory.png");
