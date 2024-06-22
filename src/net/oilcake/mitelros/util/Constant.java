bdd1d90bee133fd44636ebcfa097feac5d3ab752
Chris <3108346541@qq.com>
2024-06-23 02:16:39 +0800

    Optimized.

--- src/net/oilcake/mitelros/util/Constant.java
+++ src/net/oilcake/mitelros/util/Constant.java
@@ -6,7 +6,7 @@ import net.minecraft.bjo;
 import net.oilcake.mitelros.item.Items;
 
 public class Constant {
-    public static final String VERSION = " R16e ";
+    public static final String VERSION = " R16f ";
     public static final bjo icons_itf = new bjo("textures/gui/icons_mitf.png");
 //    public static final bjo MITE_icons = new bjo("textures/gui/MITE_icons.png");
 //    public static final bjo inventory = new bjo("textures/gui/inventory.png");
